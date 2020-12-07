55ab3aa0c6e8c75d16c04f956f745b07c8c7f44e
Ajay Marathe <ajay@learny.co>
2020-12-07 20:59:48 +0530

    implemnted gifted chat feature with firebase

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
 // In App.js in a new project
 
 import * as React from 'react';
-// import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Button, View, Text } from 'native-base';
 import Chat from './Components/Chat';
+import { TextInput } from 'react-native';
 
 function HomeScreen({ navigation }) {
-  console.log('props', navigation);
+  const [value, onChangeText] = React.useState('');
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', alignSelf: 'center' }}>
-      <Button style={{ borderWidth: 1}} primary onPress={() => navigation.navigate('Chat')} >
-        <Text>Chat!</Text>
+    <View style={{ flex: 1, width: '100%', alignItems: 'center', justifyContent: 'center', alignSelf: 'center' }}>
+      <TextInput
+        style={{ width: '100%', borderWidth: 1, }}
+        onChangeText={text => onChangeText(text)}
+        value={value}
+      />
+      <Button style={{ borderWidth: 1 }} primary onPress={() => navigation.navigate('Chat',{
+            id: 1,
+            name: value,
+          })} >
+        <Text>Enter in Chat!</Text>
       </Button>
     </View>
   );
--- Components/Chat.js
+++ Components/Chat.js
@@ -1,13 +1,73 @@
 import React, { useState, useCallback, useEffect } from 'react'
-import { GiftedChat } from 'react-native-gifted-chat'
+import { GiftedChat } from 'react-native-gifted-chat';
+import firebase from "@react-native-firebase/app";
+// import database from '@react-native-firebase/database';
+import auth from '@react-native-firebase/auth';
 
-function Chat() {
+function Chat({ route, }) {
+  console.log('name:', route.params);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
+    // firebase
+    auth()
+      .signInAnonymously()
+      .then(() => {
+        //get messages from db
+        //order them by date of creation
+        firebase
+          .database()
+          .ref("chats")
+          .orderByChild("createdAt")
+          .on("child_added", snap => {
+            //child_added listens to the new updates at the chats node
+            // console.log(snap);
+            let data = snap.val();
+            console.log('snap', snap);
+
+            this.setState(prev => ({
+              isAuthenticated: true,
+              messages: GiftedChat.append(prev.messages, data)
+            }));
+          });
+      })
+      .catch(err => {
+        console.log("err:" + JSON.stringify(err))
+      });
+
     setMessages([
       {
         _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 3,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 4,
         text: 'Hello developer',
         createdAt: new Date(),
         user: {
@@ -20,7 +80,16 @@ function Chat() {
   }, [])
 
   const onSend = useCallback((messages = []) => {
+
     setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+
+    firebase
+      .database()
+      .ref("/chats")
+      .push(messages[0])
+      .catch(err => {
+        alert("failed to send message:" + err);
+      });
   }, [])
 
   return (
@@ -28,7 +97,9 @@ function Chat() {
       messages={messages}
       onSend={messages => onSend(messages)}
       user={{
-        _id: 1,
+        _id: route.params.id,
+        name: route.params.name,
+        avatar: 'https://placeimg.com/140/140/any',
       }}
     />
   )
